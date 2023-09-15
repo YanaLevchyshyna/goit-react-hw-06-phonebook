5c00a7d940b76bb7b74f4e8b823420a73adc9bc3
Yana-Levchyshyna <levchyshynayana@gmail.com>
2023-09-15 16:17:56 +0300

    commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,32 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { contactsReducer } from 'redux/contactsSlice';
-import { formReducer } from 'redux/formSlice';
-import { filterReducer } from 'redux/filterSlice';
+import { reducer } from 'redux/redusers';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedContactsReducer = persistReducer(persistConfig, reducer);
 
 export const store = configureStore({
-  reducer: {
-    contactsForm: formReducer,
-    contacts: contactsReducer,
-    filter: filterReducer,
-  },
+  reducer: persistedContactsReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
